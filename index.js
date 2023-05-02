4e37cc4f865c7d223bb86870dc22df1a45cc3c47
Shahidul Islam <shahidulislam@Konas-iMac.local>
2023-05-03 00:53:12 +0600

    images updated

--- index.js
+++ index.js
@@ -1,31 +1,28 @@
+
+
 const express = require('express');
 const app = express();
-const cors = require('cors')
-const port =process.env.PORT || 8000 ;
-app.use(cors())
+const cors = require('cors');
+const port = process.env.PORT || 6001;
 
-const datas = require('./Data/data.json')
 
-app.get('/', (req, res) => {
-    res.send('Hello eveyone');
-})
+app.use(cors());
+const datas = require('./Data.json')
+app.get('/', (req, res) =>{
+    res.send('Server is running')
+});
+
+
 app.get('/datas',(req, res)=>{
     res.send(datas)
 })
 
 app.get('/recipes/:id', (req,res)=>{
-    const id = req.params.id;
-    const singleData = datas.find(d => d.id == id)
-    res.send(singleData)
+        const id = req.params.id;
+        const singleData = datas.find(d => d.id == id)
+        res.send(singleData)
 })
 
-
-app.get('/data', (req, res)=>{
-   res.send('Hi everuone')
-})
-
-
-
-app.listen(port, ()=>{
-console.log(`Server is running in port ${port}`)
+app.listen(port, () => {
+    console.log(`Server is running on port: ${port}`)
 })
